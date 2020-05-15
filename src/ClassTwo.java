448f0f8332fdb59d8b42242f59c797fa6f85d0ed
JackMcCabe548 <jackmccabe628@yahoo.co.uk>
2020-05-15 13:19:20 +0100

    Version 0.11

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class ClassTwo {
+
+    ClassTwo(){
+        String FirstNum = JOptionPane.showInputDialog("Enter first number");
+        String SecondNum = JOptionPane.showInputDialog("Enter second Number");
+        int First = Integer.parseInt(FirstNum); // Convert user input to int
+        int Second = Integer.parseInt(SecondNum); // convert user intput to int
+        int Total = First + Second;
+
+        JOptionPane.showMessageDialog(null, "The output is " + Total);
+    }
+
 }
